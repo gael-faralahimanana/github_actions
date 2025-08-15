e759e5c64468ad555aaacfa53c9c8aa871538c27
Ubuntu <ubuntu@ip-172-31-22-200.eu-west-1.compute.internal>
2025-08-15 07:58:46 +0000

    Déplace et corrige les tests pour flake8/pytest

---
+++ tests/file_test.py
@@ -0,0 +1,38 @@
+def test_calc_addition():
+
+    """Vérifie que 2 + 4 == 6."""
+
+    output = 2 + 4
+
+    assert output == 6
+
+
+
+def test_calc_subtraction():
+
+    """Vérifie que 2 - 4 == -2."""
+
+    output = 2 - 4
+
+    assert output == -2
+
+
+
+def test_calc_multiply():
+
+    """Vérifie que 2 * 4 == 8."""
+
+    output = 2 * 4
+
+    assert output == 8
+
+
+
+def test_coucou():
+
+    """Vérifie que la sortie est 'hello'."""
+
+    output = "hello"
+
+    assert output == "hello"
+
